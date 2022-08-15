f59366c0c98bd8ecda6063c6d8278e7c60d53837
Azimov Gleb <36067955+Glebaz66@users.noreply.github.com>
2022-08-16 01:52:45 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -7,81 +7,107 @@ class FormHandler {
     constructor(form){
         if(form === null) return
 
+        this.body = document.body
         this.form = form
-        this.email = this.form.querySelector('input[name="email"]')
-        this.password = this.form.querySelector('input[name="password"]')
-        this.submit = this.form.querySelector('.js-submit')
-        this.error = this.form.querySelectorAll('.form__error')
-        this.isValid = false
-        this.data = {
-            email: this.email.value,
-            password: this.password.value,
+        this.input = {
+            email: {
+                elem: this.form.querySelector('input[type="email"]'),
+                value: '',
+                valid: false
+            },
+            password: {
+                elem: this.form.querySelector('input[type="password"]'),
+                value: '',
+                valid: false
+            }
         }
+        this.submit = this.form.querySelector('.js-submit')
 
-        this.submit.addEventListener('click', (e) => {
-            e.preventDefault()
-
-            if(this.isValid === true){
-                this.sendForm()
+        
+        this.input.email.elem.addEventListener('keypress', (e) => {
+            if(e.key === 'Enter') {
+                this.validateInput(this.input.email.elem)
             }
         })
-
-        Array.from([this.email, this.password]).forEach(elem => {
-
-            elem.addEventListener('keypress', (e) => { this.validateOnKeyPress(e, elem) })
-
-            elem.addEventListener('click', () => { 
-                this.handleFocus(elem)
-             })
-
-            elem.addEventListener('focus', () => {
-                elem.classList.remove('focus')
-                elem.classList.remove('error')
-                elem.nextElementSibling.classList.remove('active')
-            })
-
-            elem.addEventListener('focusout', () => {
-                elem.classList.remove('focus')
-                elem.classList.remove('error')
-                elem.nextElementSibling.classList.remove('active')
-
-                this.validator(elem)
-                
-                this.setValidiry(this.isValid, elem)
-
-            })
+        this.input.password.elem.addEventListener('keypress', (e) => {
+            if(e.key === 'Enter') {
+                this.validateInput(this.input.password.elem)
+            }
+        })
+        this.input.email.elem.addEventListener('keydown', (e) => {
+            if(e.which === 9) {
+                this.validateInput(this.input.email.elem)
+            }
+        })
+        this.input.password.elem.addEventListener('keydown', (e) => {
+            if(e.which === 9) {
+                this.validateInput(this.input.password.elem)
+            }
+        })
+        
+        this.input.email.elem.addEventListener('focus', () => {
+            this.removeError(this.input.email.elem)
+        })
+        
+        this.input.password.elem.addEventListener('focus', () => {
+            this.removeError(this.input.password.elem)
+        })
+        
+        this.body.addEventListener('click' , (e) => {
+            if(!e.target.classList.contains('js-submit')){
+                this.removeError(this.input.email.elem)
+                this.removeError(this.input.password.elem)
+            }
+        })
+        this.submit.addEventListener('click', (e) => {
+            e.preventDefault()
+            this.sendForm(this.input)
         })
     }
 
-    handleFocus(elem){
-        elem.classList.add('focus')
-    }
+    validateInput(elem){
+        const type = elem.dataset.type;
+        const value = elem.value;
+        let isValid = false;
+        switch (type) {
+            case 'email':
+                const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/;
+                const match = value.match(pattern);
 
-    validator(input){
-        let pattern;
-        const type = input.dataset.type
-        type === 'email' ? pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/ : pattern = ''
-        const value = input.value
-        const match = value.match(pattern)
+                if(match === null || value === '') {
+                    console.log(elem);
+                    this.input.email.valid = false
+                    this.toggleError(this.input.email.valid, elem)
+                } else {
+                    this.input.email.valid = true
+                    this.toggleError(this.input.email.valid, elem)
+                    isValid = true
+                }
 
-        if(input.value === '' || match === null){
-            return this.isValid = false
-        }
+                return isValid;
+
+                case 'password':
+                    value === '' ? this.input.password.valid = false : this.input.password.valid = true
+
+                    if(value === ''){
+                        this.input.password.valid = false
+                        this.toggleError(this.input.password.valid, elem)
+                    } else {
+                        this.input.password.valid = true
+                        this.toggleError(this.input.password.valid, elem)
+                        isValid = true
+                    }
+                    return isValid;
         
-        return this.isValid = true
+            default:
+                this.input.password.valid = false
+                this.toggleError(this.input.password.valid, elem)
+                return isValid;
+        }
     }
 
-    validateOnKeyPress(e, input){
-        if(e.key === 'Enter'){
-            e.preventDefault()
-            this.validator(input)
-            this.setValidiry(this.isValid, input)
-        }
-
-    };
-
-    setValidiry(isValid, elem){
-        if(isValid === false){
+    toggleError(isValid, elem){
+        if(!isValid){
             elem.classList.add('error')
             elem.nextElementSibling.classList.add('active')
         } else {
@@ -89,9 +115,30 @@ class FormHandler {
             elem.nextElementSibling.classList.remove('active')
         }
     }
-    sendForm(){
-        this.data.email = this.email.value
-        this.data.password = this.password.value
-        this.form.submit()
+
+    removeError(elem){
+        elem.classList.remove('error')
+        elem.nextElementSibling.classList.remove('active')
+    }
+
+    async sendForm(){
+
+        if( this.validateInput(this.input.email.elem) === true &&
+            this.validateInput(this.input.password.elem) === true ) {
+                alert('data sended')
+                this.form.submit()
+            }
+        
+        // const data = new FormData
+
+        // data.append('email', this.input.email.value)
+        // data.append('password', this.input.password.value)
+
+        // const req = await fetch(url, {
+        //     method: 'post',
+        //     body: data
+        // });
+
+        // const resp = await req.json()
     }
 }
